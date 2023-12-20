222c3c882d700938c65c08815c1e6aae880b5bd5
elephantchan <474116207@qq.com>
2023-12-20 21:10:36 +0800

    feat：创建应用

---
+++ StealthRocketVPN/StealthRocketVPN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StealthRocketVPN
+//
+//  Created by Kuntal Sheth on 12/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
